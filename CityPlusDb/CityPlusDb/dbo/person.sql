3c2144ad12f1e9c3460c52649484b8580d15d623
Jef King <jefkin@microsoft.com>
2017-03-08 12:36:31 -0800

    person table

---
+++ CityPlusDb/CityPlusDb/dbo/person.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[person]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[location] geography NOT NULL
+)
--- CityPlusDb/CityPlusDb/dbo/resources.sql
+++ CityPlusDb/CityPlusDb/dbo/resources.sql
@@ -3,5 +3,6 @@
 	[id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
     [description] NVARCHAR(MAX) NOT NULL, 
     [quantity] INT NOT NULL DEFAULT 0, 
-    [category] NVARCHAR(255) NOT NULL DEFAULT 'unknown'
+    [category] NVARCHAR(255) NOT NULL DEFAULT 'unknown',
+	[location] geography NOT NULL
 )
