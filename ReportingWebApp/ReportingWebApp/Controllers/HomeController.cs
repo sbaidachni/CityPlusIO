233407ac49776c6b01f32525f4461314ad73fff5
Sergii Baidachnyi <sbaidachni@gmail.com>
2017-03-07 19:34:13 -0800

    Report functionality is added

--- ReportingWebApp/ReportingWebApp/Controllers/HomeController.cs
+++ ReportingWebApp/ReportingWebApp/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.PowerBI.Api.V1;
+using Microsoft.PowerBI.Security;
+using Microsoft.Rest;
+using ReportingWebApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +14,21 @@ namespace ReportingWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string workspaceCollection;
+        private readonly string workspaceId;
+        private readonly string workspaceId2;
+        private readonly string accessKey;
+        private readonly string apiUrl;
+
+        public HomeController()
+        {
+            this.workspaceCollection = ConfigurationManager.AppSettings["powerbi:WorkspaceCollection"];
+            this.workspaceId = ConfigurationManager.AppSettings["powerbi:WorkspaceId"];
+            this.workspaceId2 = ConfigurationManager.AppSettings["powerbi:WorkspaceId2"];
+            this.accessKey = ConfigurationManager.AppSettings["powerbi:AccessKey"];
+            this.apiUrl = ConfigurationManager.AppSettings["powerbi:ApiUrl"];
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +46,57 @@ namespace ReportingWebApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Reports()
+        {
+            using (var client = this.CreatePowerBIClient())
+            {
+                var reportsResponse = client.Reports.GetReports(this.workspaceCollection, this.workspaceId);
+
+                var viewModel = new ReportsViewModel
+                {
+                    Reports = reportsResponse.Value.ToList(),
+                    ReportWorkspace = new System.Collections.Generic.Dictionary<string, string>()
+                };
+
+                foreach (var r in viewModel.Reports)
+                {
+                    viewModel.ReportWorkspace.Add(r.Id, this.workspaceId);
+                }
+
+                return PartialView(viewModel);
+            }
+        }
+
+        public async Task<ActionResult> Report(string reportId, string workspaceSId)
+        {
+            using (var client = this.CreatePowerBIClient())
+            {
+                var reportsResponse = await client.Reports.GetReportsAsync(this.workspaceCollection, workspaceSId);
+                var report = reportsResponse.Value.FirstOrDefault(r => r.Id == reportId);
+                var embedToken = PowerBIToken.CreateReportEmbedToken(this.workspaceCollection, workspaceSId, report.Id);
+
+                var viewModel = new ReportViewModel
+                {
+                    Report = report,
+                    AccessToken = embedToken.Generate(this.accessKey)
+                };
+
+                return View(viewModel);
+            }
+        }
+
+        private IPowerBIClient CreatePowerBIClient()
+        {
+            var credentials = new TokenCredentials(accessKey, "AppKey");
+            var client = new PowerBIClient(credentials)
+            {
+                BaseUri = new Uri(apiUrl)
+            };
+
+            return client;
         }
     }
 }
---
+++ ReportingWebApp/ReportingWebApp/Models/ReportsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.PowerBI.Api.V1.Models;
+
+namespace ReportingWebApp.Models
+{
+    public class ReportsViewModel
+    {
+        public List<Report> Reports { get; set; }
+
+        public Dictionary<string, string> ReportWorkspace { get; set; }
+    }
+}
