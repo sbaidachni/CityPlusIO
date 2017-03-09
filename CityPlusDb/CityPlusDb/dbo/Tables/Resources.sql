8b2eb3c3e5a7d7aa49675fe73e0f765ed991c31b
Jef King <jefkin@microsoft.com>
2017-03-09 15:49:25 -0800

    data base

---
+++ CityPlusDb/CityPlusDb/dbo/Tables/Resources.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Resources] (
+    [Name]     NVARCHAR (100) NULL,
+    [Location] NVARCHAR (100) NULL,
+    [Food]     NVARCHAR (50)  NULL,
+    [Shelter]  NVARCHAR (50)  NULL,
+    [Clothes]  NVARCHAR (50)  NULL,
+    [Medicine] NVARCHAR (50)  NULL
+);
+
